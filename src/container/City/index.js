d477142e15e7c9191a473729c49a2d52c5f3be9d
简涛 <jiantao@pp100.com>
2018-08-27 18:48:55 +0800

    表单使用

--- src/container/City/index.js
+++ src/container/City/index.js
@@ -11,7 +11,30 @@ class City extends Component {
         return (
             <div>
                 <Card>
-                    <FormItem></FormItem>
+                    <Form layout="inline">
+                        <FormItem label="城市">
+                            <Select
+                                style={{width:100}}
+                                placeholder="全部"
+                            >
+                                <option value="">全部</option>
+                                <option value="1">北京市</option>
+                                <option value="2">天津市</option>
+                                <option value="3">深圳市</option>
+                            </Select>
+                        </FormItem>
+                        <FormItem label="运营模式">
+                            <Select
+                                style={{width:100}}
+                                placeholder="全部"
+                            >
+                                <option value="">全部</option>
+                                <option value="1">北京市</option>
+                                <option value="2">天津市</option>
+                                <option value="3">深圳市</option>
+                            </Select>
+                        </FormItem>
+                    </Form>
                 </Card>
             </div>
         )
